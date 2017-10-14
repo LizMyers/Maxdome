7850fa677e0381b56fb7ade79e8a7560b43fb8d1
Liz Myers <liz@myersdesign.com>
2017-10-14 07:40:57 +0200

    Delete maxdome-headers.js

--- maxdome-headers.js
+++
@@ -1,9 +0,0 @@
-headers: {
-    accept:'application/json',
-    client:'mxd_store',
-    clienttype:'webportal',
-    'content-type':'application/json',
-    language:'de_DE',
-    'maxdome-origin':'maxdome.de',
-    platform:'web'
-}
